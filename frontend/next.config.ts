95c70810befab9a33ba273e35fe9349a97e8948a
Leonardo Lavigne <lclavignept@gmail.com>
2025-12-07 23:45:02 +0000

    Refactor(Frontend): Migrate middleware to proxy and configure dynamic allowed origins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["localhost:3000", "127.0.0.1:3000", "0.0.0.0:3000"];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: allowedOrigins,
+    },
+  },
+  allowedDevOrigins: allowedOrigins,
 };
 
 export default nextConfig;
